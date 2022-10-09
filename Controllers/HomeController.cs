46fd209ed942432f1d106686f18d6188564f08ed
abdelmhe <92818538+abdelmhe@users.noreply.github.com>
2022-10-09 00:52:11 -0400

    added a repository class, created home controller action methods for request form added http get and post annotations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,33 @@ namespace ITEquipReq.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
-    {
+    [HttpGet]
+    public ViewResult RequestForm(){
         return View();
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    public ViewResult RequestForm(FormResponse formResponse)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        if (ModelState.IsValid){
+            Repository.AddResponse(formResponse);
+            return View("Confirmation", formResponse);
+        }else{
+            return View();
+        }
+
+
     }
+    // public ViewResult ListResponses()
+    
+    // {
+    //     return View(Repository.Responses.Where(r => r.willAttend == true));
+
+
+    // }
+    
 }
